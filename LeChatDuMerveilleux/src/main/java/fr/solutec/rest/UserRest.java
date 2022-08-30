16755ef53ab3884093d53d7f5a266ab3ba66f647
dsi <dsi@A0003948>
2022-08-30 15:46:01 +0200

    Login API

--- LeChatDuMerveilleux/src/main/java/fr/solutec/rest/UserRest.java
+++ LeChatDuMerveilleux/src/main/java/fr/solutec/rest/UserRest.java
@@ -1,14 +1,26 @@
 package fr.solutec.rest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.solutec.entities.User;
 import fr.solutec.repository.UserRepository;
 
 @RestController @CrossOrigin("*")
+
 public class UserRest {
+	
 	@Autowired
 	private UserRepository userRepo;
+	
+	@PostMapping("login")
+	public Optional <User> getLoginAndPassword(@RequestBody User u) {			
+		return userRepo.findByLoginAndPassword(u.getLogin(),u.getPassword());					
+	}	
 	
 }
