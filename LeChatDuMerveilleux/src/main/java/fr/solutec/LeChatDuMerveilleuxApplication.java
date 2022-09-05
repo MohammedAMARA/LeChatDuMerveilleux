3c36c14fc860e095187756d1b8fd884a4ea769d8
adrienpillou <adrien.pillou@hotmail.fr>
2022-09-05 20:34:21 +0200

    Dernier commit de Lundi

--- LeChatDuMerveilleux/src/main/java/fr/solutec/LeChatDuMerveilleuxApplication.java
+++ LeChatDuMerveilleux/src/main/java/fr/solutec/LeChatDuMerveilleuxApplication.java
@@ -41,10 +41,10 @@ public class LeChatDuMerveilleuxApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		System.out.println("Lancement terminé");
 		
-		User mohammed = new User(null, "Momo", "login1", "password1","0601020304",50,null); 
-		User guillaume = new User(null, "Guigui", "login2", "password2", "0611121314", 40, null); 
-		User adrien = new User(null, "Dridri", "login3", "password3", "0621222324", 60, null); 
-		User shajiesan = new User(null, "Sai", "login4", "password4", "0631323334", 100, null);	
+		User mohammed = new User(null, "Momo", "login1", "password1","0601020304",50, "https://avatars.dicebear.com/api/bottts/17508.svg"); 
+		User guillaume = new User(null, "Guigui", "login2", "password2", "0611121314", 40,  "https://avatars.dicebear.com/api/bottts/15432.svg"); 
+		User adrien = new User(null, "Dridri", "login3", "password3", "0621222324", 60,  "https://avatars.dicebear.com/api/bottts/42345.svg"); 
+		User shajiesan = new User(null, "Sai", "login4", "password4", "0631323334", 100,  "https://avatars.dicebear.com/api/bottts/56722.svg");	
 		userRepo.save(mohammed);
 		userRepo.save(guillaume);
 		userRepo.save(adrien);
--- LeChatDuMerveilleux/src/main/java/fr/solutec/rest/UserRest.java
+++ LeChatDuMerveilleux/src/main/java/fr/solutec/rest/UserRest.java
@@ -1,5 +1,7 @@
 package fr.solutec.rest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,8 +49,5 @@ public class UserRest {
 		return userRepo.save(user);
 		
 	}
-		
-	
-	
 	
 }
